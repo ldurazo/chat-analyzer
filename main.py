993cf04a16ade24e5a22f15c5d19fe88debd1c2a
ldurazo <ldurazo-contractor@beyond.ai>
2020-10-03 12:56:20 -0700

    adds some more information about text, emojis, and words

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import pandas as pd
-df = pd.read_json('./data/telegram.json')
-print('a')
+import emoji
+
+
+def get_emojis_in_message(row):
+    message = row.text
+    emojis = ""
+    if message is None or type(message) != str:
+        return emojis
+    return emojis.join(c for c in message if c in emoji.UNICODE_EMOJI)
+
+
+class Row:
+    text = "Hello 🤭"
+
+print(get_emojis_in_message(Row()))
